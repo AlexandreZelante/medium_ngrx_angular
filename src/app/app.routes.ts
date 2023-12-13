7f355a6d179d6a686a6d8546dc640ed110e9471d
Alexandre Zelante <alexandre.zelante@outlook.com>
2023-12-13 00:23:27 -0300

    feat: global feed component and store

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,4 +13,11 @@ export const appRoutes: Route[] = [
     loadChildren: () =>
       import('src/app/auth/auth.routes').then((m) => m.loginRoutes),
   },
+  {
+    path: '',
+    loadChildren: () =>
+      import('src/app/globalFeed/globalFeed.routes').then(
+        (m) => m.globalFeedRoutes
+      ),
+  },
 ];
---
+++ src/app/shared/types/article.interface.ts
@@ -0,0 +1,11 @@
+export interface ArticleInterface {
+  body: string;
+  createdAt: string;
+  description: string;
+  favorited: boolean;
+  favoritedCount: number;
+  slug: string;
+  tagList: string[];
+  title: string;
+  updatedAt: string;
+}
